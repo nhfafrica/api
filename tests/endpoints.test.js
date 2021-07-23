a806f7c1dd126e69b2ee2014896b44fbfbb93c01
Chiamaka Osumgba <osumgbachiamaka@gmail.com>
2021-07-23 06:58:48 +0100

    WIP: test enpoints with jest and supertest

--- tests/endpoints.test.js
+++ tests/endpoints.test.js
@@ -1,9 +1,41 @@
 const app = require('../routes/app')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+
 require('dotenv').config()
 
-const uri = process.env.TEST_DB_CONNECTION
+const uri = process.env.DB_CONNECTION
+const mockUser = {
+  "firstName": "me",
+  "lastName": "nothing",
+  "email": "me@gmail.com",
+  "phoneNumber": "08123944502",
+  "additionalData": {
+    "physicalAddress": "No 1 off road",
+    "pronoun": "You/Me",
+    "birthDay": "20-09-2021",
+    "occupation": "building this"
+  },
+  "socialBackground": {
+    "socialMediaHandles": {
+        "twitterHandle": "",
+        "instagramHandle": "",
+        "facebookHandle": "",
+        "linkedinHandle": ""
+    },
+    "publicFigures": "myself",
+    "mostAdmiredNGO": "mine"
+  },
+  "interests": {
+      "preferedSocialMedia": "twitter",
+      "volunteerType": "what do you want",
+      "interestedVolunteerUnits": ["let's negotiate"]
+  },
+    "reasonForVolunteering": "Help humanity"
+  }
+
+beforeAll(() => jest.setTimeout(8000 * 8000))
+
 beforeEach((done) => {
     mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true}, () => done())
 })
@@ -13,9 +45,47 @@ afterEach((done) => {
     mongoose.connection.close(() => done())
   })
 })
-describe('API end point test', () => {
-    it('testing the GET end point', () => {
 
-    })
+describe('API POST endpoint test', () => {
+  it('POST endpoint should pass', async () => {
+    const response = await supertest(app)
+      .post('/api/v1/volunteer-applicants')
+      .send(mockUser)
+      .expect(201)
+    expect(response.statusCode).toEqual(201)
+    expect(response.body.message).toBe('Registration Successful')
+  })
+
+  it('POST endpoint should fail', async () => {
+    const response = await supertest(app)
+      .post('/api/v1/volunteer-applicants')
+      .send({'firstName': 'chiamaka'})
+      .expect(400)
+    expect(response.statusCode).toEqual(400)
+    expect(response.text.includes('error')).toBeTruthy()
+  })
+
 })
 
+describe('API GET endpoint test', () => {
+  it('GET endpoint should fail', async () => {
+    const response = await supertest(app)
+      .get('/api/v1/volunteer-applicants')
+      .set('Accept', 'application/doc')
+      .expect('Content-Type', '/json/')
+      .expect(400)
+      console.log(response)
+  })
+
+  it('GET endpoint should pass', async () => {
+    const response = await supertest(app)
+      .get('/api/v1/volunteer-applicants')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', '/json/')
+      .expect(200)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(err => console.log(console.log(err)))
+  })
+})
